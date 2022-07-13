fa2906c9d3e2b9d3db156b804ea08cc713852e9d
NAYANQA <nayanqa.om@gmail.com>
2022-07-13 22:28:06 +0100

    data created

---
+++ src/Primitive_data_types/Floting_Points.java
@@ -0,0 +1,13 @@
+package Primitive_data_types;
+
+public class Floting_Points{
+        public static void main(String[] args){
+    float a = 1f;
+    double b = 1.250f;
+            float c = (float) (b / a);
+
+   // System.out.println(a);
+   // System.out.println(b);
+    System.out.println("PRint valuse of c=" +c);
+        }
+}
